20015031f8cfb9aee5d3c408b73f80068d4f53e4
Steve Roudebush <steven.roudebush@rapidrobotics.com>
2022-05-25 13:45:51 +0100

    setup working test

---
+++ empty-test-proj/Assets/Scripts/BasicFunctions.cs
@@ -0,0 +1,11 @@
+﻿namespace User {
+public static class Math {
+    public static float Add(float lhs, float rhs) {
+        return lhs + rhs;
+    }
+
+    public static int Add(int lhs, int rhs) {
+        return lhs + rhs;
+    }
+}
+}
---
+++ empty-test-proj/Assets/Tests/BasicTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using User;
+
+namespace Tests {
+public class BasicTests {
+    [Test]
+    public void AdditionShouldWork() {
+        var lhs = 4;
+        var rhs = 5;
+        var expected = 9;
+        var result = Math.Add(lhs, rhs);
+
+        Assert.AreEqual(expected, result);
+    }
+}
+}
