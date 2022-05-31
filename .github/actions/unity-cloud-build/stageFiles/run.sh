17dc77065ca377a3e80918d35656354ee79239d0
Steve Roudebush <steven.roudebush@rapidrobotics.com>
2022-05-31 11:28:08 -0500

    polish

--- .github/actions/unity-cloud-build/stageFiles/run.sh
+++ .github/actions/unity-cloud-build/stageFiles/run.sh
@@ -3,20 +3,20 @@
 PROJECT_NAME="default-webgl"
 
 mkdir -p work
-mkdir -p dist
+mkdir -p dist/Build
 
 echo "downloading url ${ARTIFACT_URL}"
 
 curl "${ARTIFACT_URL}" --output work/artifact.zip
 
-7z x -odist work/artifact.zip
-
+### PUBLISH ALL ARTIFACTS
 #7z x -odist work/artifact.zip
+### END
 
-#7z x -odist work/${PROJECT_NAME}/Build/${PROJECT_NAME}.data.gz
-#7z x -odist work/${PROJECT_NAME}/Build/${PROJECT_NAME}.framework.js.gz
-#7z x -odist work/${PROJECT_NAME}/Build/${PROJECT_NAME}.wasm.gz
-#mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.loader.js dist/${PROJECT_NAME}.loader.js
-#mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.data dist/${PROJECT_NAME}.data
-#mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.wasm dist/${PROJECT_NAME}.wasm
-#mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.framework.js dist/${PROJECT_NAME}.framework.js
+#### PUBLISH SPECIFIC ARTIFACTS
+7z x -owork work/artifact.zip
+mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.loader.js dist/Build/${PROJECT_NAME}.loader.js
+mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.data dist/Build/${PROJECT_NAME}.data
+mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.wasm dist/Build/${PROJECT_NAME}.wasm
+mv work/${PROJECT_NAME}/Build/${PROJECT_NAME}.framework.js dist/Build/${PROJECT_NAME}.framework.js
+#### END
